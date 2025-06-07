ca693ae352c839c22bd9398bb86c2543e9665779
rabiu <rabiul17.ri@gmail.com>
2025-06-07 17:27:12 +0600

    clock and date

--- lib/clock_and_time.dart
+++ lib/clock_and_time.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
 class clock_and_time extends StatefulWidget {
   const clock_and_time({super.key});
 
@@ -8,32 +8,71 @@ class clock_and_time extends StatefulWidget {
 }
 
 class _clock_and_timeState extends State<clock_and_time> {
+
+  late Timer _timer;
+  late DateTime _currentTime;
+
+  final List<String> _monthName = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+  @override
+  void initState(){
+    super.initState();
+    _currentTime = DateTime.now();
+    _timer = Timer.periodic(Duration(milliseconds: 1), (Timer t){
+      setState(() {
+        _currentTime = DateTime.now();
+      });
+    });
+  }
+
+
+  @override
+  void dispose(){
+    _timer.cancel();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    var time = DateTime.now();
+    bool cat = false;
+    Color cardColor = (_currentTime.millisecond > 1 && _currentTime.millisecond < 500 )
+    ? Colors.red : Colors.green;
+   // var time = DateTime.now();
+    String monthName = _monthName[_currentTime.month - 1];
     return Scaffold(
+
+      backgroundColor: Colors.deepOrange,
+
       appBar: AppBar(
-        title: Text('Date and Time Clock'),
+        centerTitle: true,
+        title: Text('Date and Time',
+        style: TextStyle(
+          color: Colors.grey,
+          fontWeight: FontWeight.bold,
+        ),),
+        backgroundColor: Colors.black,
       ),
 
       body: Center(
+
         child: Container(
           width: 200,
           height: 200,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Time: ${time.hour}:${time.minute}:${time.second}:',
-              style: TextStyle(
-                fontSize: 30,
-              ),),
-              ElevatedButton(onPressed: (){
-                setState(() {
-
-                });
-              },
-                  child: Text('Current Time'))
-            ],
+          child: Card(
+           color: cardColor,
+            elevation: 15,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('${_currentTime.day} $monthName, ${_currentTime.year} \n ${_currentTime.hour} : ${_currentTime.minute} : ${_currentTime.second} \n ms-${_currentTime.millisecond}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
